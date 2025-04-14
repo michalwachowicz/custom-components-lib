3a3f1c22accf151592d3d19d584249744d3c556a
michalwachowicz <wachowiczmichal1@gmail.com>
2025-04-14 12:15:10 +0200

    feat: create a textfield component

---
+++ src/components/textfield/TextField.test.tsx
@@ -0,0 +1,131 @@
+import { act } from "react";
+import { render, screen } from "@testing-library/react";
+import TextField, { TextFieldVariant } from "./TextField";
+import userEvent from "@testing-library/user-event";
+
+describe("<TextField />", () => {
+  it("inputs text correctly", async () => {
+    render(<TextField label='Name' />);
+
+    const input = screen.getByRole("textbox");
+    const user = userEvent.setup();
+
+    expect(input).toHaveValue("");
+
+    await act(async () => {
+      await user.type(input, "test");
+    });
+
+    expect(screen.getByRole("textbox")).toHaveValue("test");
+  });
+
+  it("does not allow input when disabled", async () => {
+    render(<TextField label='Name' disabled />);
+
+    const input = screen.getByRole("textbox");
+    const user = userEvent.setup();
+
+    expect(input).toHaveValue("");
+    expect(input).toBeDisabled();
+
+    await act(async () => {
+      await user.type(input, "test");
+    });
+
+    expect(screen.getByRole("textbox")).toHaveValue("");
+  });
+
+  it("renders label correctly", () => {
+    render(<TextField label='Name' />);
+    expect(screen.getByLabelText("Name")).toBeInTheDocument();
+  });
+
+  it("applies error class if error prop is true", () => {
+    render(<TextField label='Name' error />);
+    expect(screen.getByTestId("textfield")).toHaveClass("textfield-error");
+  });
+
+  it("shows error message if error prop is a string", () => {
+    render(<TextField label='Name' error='Incorrect input.' />);
+
+    expect(screen.getByTestId("textfield")).toHaveClass("textfield-error");
+    expect(screen.getByText("Incorrect input.")).toBeInTheDocument();
+  });
+
+  it("applies focused class when input is focused", async () => {
+    render(<TextField label='Name' />);
+
+    const input = screen.getByRole("textbox");
+    const user = userEvent.setup();
+    const focusedClass = "textfield-focused";
+
+    expect(screen.getByTestId("textfield")).not.toHaveClass(focusedClass);
+
+    await act(async () => {
+      await user.click(input);
+    });
+
+    expect(screen.getByTestId("textfield")).toHaveClass(focusedClass);
+  });
+
+  it("shows legend when variant is outlined and focused or not empty", async () => {
+    render(
+      <div>
+        <TextField label='Name' variant='outlined' />
+        <div data-testid='outside' />
+      </div>
+    );
+
+    const input = screen.getByRole("textbox");
+    const user = userEvent.setup();
+
+    expect(screen.queryByTestId("textfield-fieldset")).not.toBeInTheDocument();
+
+    await act(async () => {
+      await user.click(input);
+    });
+
+    expect(screen.getByTestId("textfield-fieldset")).toBeInTheDocument();
+
+    await act(async () => {
+      await user.click(screen.getByTestId("outside"));
+    });
+
+    expect(screen.queryByTestId("textfield-fieldset")).not.toBeInTheDocument();
+
+    await act(async () => {
+      await user.type(input, "test");
+      await user.click(screen.getByTestId("outside"));
+    });
+
+    expect(screen.getByTestId("textfield-fieldset")).toBeInTheDocument();
+  });
+
+  describe("variants", () => {
+    const variants = ["standard", "outlined", "filled"];
+    const errorStates = [true, false, "error message"];
+    const disabledStates = [true, false];
+
+    const testCases = variants.flatMap((variant) =>
+      errorStates.flatMap((error) =>
+        disabledStates.map((disabled) => [variant, error, disabled])
+      )
+    );
+
+    test.each(testCases)(
+      "renders correctly with variant=%s, error=%s, disabled=%s",
+      (variant, error, disabled) => {
+        const { asFragment } = render(
+          <TextField
+            label='Name'
+            variant={variant as TextFieldVariant}
+            error={error}
+            disabled={!!disabled}
+          />
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+      }
+    );
+  });
+});
---
+++ src/components/textfield/TextField.tsx
@@ -0,0 +1,73 @@
+import { useId, useState } from "react";
+import * as styles from "./TextField.module.scss";
+
+export type TextFieldVariant = "filled" | "outlined" | "standard";
+
+interface TextFieldProps {
+  label: string;
+  variant?: TextFieldVariant;
+  disabled?: boolean;
+  error?: boolean | string;
+}
+
+export default function TextField({
+  label,
+  variant = "standard",
+  disabled = false,
+  error = false,
+}: TextFieldProps) {
+  const id = useId();
+
+  const [focused, setFocused] = useState(false);
+  const [value, setValue] = useState("");
+
+  const focusedClass = focused ? styles["textfield-focused"] : "";
+  const notEmptyClass = value !== "" ? styles["textfield-not-empty"] : "";
+  const errorClass = error ? styles["textfield-error"] : "";
+
+  return (
+    <div
+      className={`${styles[`textfield-container-${variant}`]}`}
+      data-testid='textfield-container'
+    >
+      <div
+        data-testid='textfield'
+        className={`${styles.textfield} ${
+          styles[`textfield-${variant}`]
+        } ${focusedClass} ${notEmptyClass} ${errorClass}`}
+      >
+        <label className={styles["textfield-label"]} htmlFor={id}>
+          {label}
+        </label>
+
+        <div className={styles["textfield-input-wrapper"]}>
+          <input
+            className={styles["textfield-input"]}
+            type='text'
+            id={id}
+            disabled={disabled}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+          />
+        </div>
+
+        {variant === "outlined" && (focused || value !== "") && (
+          <fieldset
+            className={styles["textfield-fieldset"]}
+            data-testid='textfield-fieldset'
+          >
+            <legend className={styles["textfield-fieldset-legend"]}>
+              {label}
+            </legend>
+          </fieldset>
+        )}
+      </div>
+
+      {typeof error === "string" && error.trim() !== "" && (
+        <div className={styles["textfield-error-message"]}>{error}</div>
+      )}
+    </div>
+  );
+}
